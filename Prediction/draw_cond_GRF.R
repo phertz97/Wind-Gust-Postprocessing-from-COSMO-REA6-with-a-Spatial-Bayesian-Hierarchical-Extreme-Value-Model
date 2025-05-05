02efd2ae847a0b29567b2b66a48c319eebaa74ad
Philipp Ertz <161842958+phertz97@users.noreply.github.com>
2025-05-05 17:13:14 +0200

    Update draw_cond_GRF.R
    Removed prediction function and removed the possibility to use the simple horizontal distance metric

--- Prediction/draw_cond_GRF.R
+++ Prediction/draw_cond_GRF.R
@@ -12,97 +12,52 @@ library("geosphere") # for Haversine formula
 library("geoR") # for matern function. Be careful to scale phi with 1/sqrt(3) and set kappa to 3/2
 library("evd") # for sampling from Gumbel distribution
 
-# Function for sampling from conditional Gumbel distribution.
-predict.gusts <- function(mus, # regression coefficients for location numerical vector 1 element per covariate
-                            sigmas, # regression coefficients for scale; numerical vector one element per covariate
-                            xmu, # covariate vectors for location, matrix with columns for each covariate
-                            xsigma  # covariate vectors for scale, matrix with columns for each covaraite
-                            )
-{
-    # Vectorized over covariate values, apply for each draw from the conditional distribution of the regression coefficient to obtain a sample.
-    # Be careful to add VMEAN again, when fitting fx-VMEAN, as the function only returns the direct predictant.
-    if (dim(xmu)[1]!= dim(xsigma)[1]){stop("Covariate vectors must have the same length.")}
-    N <- dim(xmu)[1]
-
-    # calculate Gumbel parameters
-    mu <- mus %*% xmu
-    sigmax <- exp(sigmas %*% xsigma)
-
-    # sammple from Gumbel distribution
-    fx_predicted <- rgumbel(n=N, loc=mu, scale=sigma)
-    return(fx_predicted)
-}
-
-
-
-# draw one representation of the conditional values at the prediction locations
-draw_cond_GRF <- function(theta, # observed values
-                  xnew, # new locations
-                  xgiven, # old locations
-                  alpha=0, # mean
+###############################
+# Simple nterpolation function
+# #############################
+draw_cond_GRF <- function(theta, # vector of observed values
+                  r.new, # coordinates for predicted location, matrix of dim [J,2] for J prediction locations
+                  r.given, # coordinated for given locations, matrixof dim [Mx2] for M known locations
+                  alpha=0, # mean 
                   sigma=1, # sill
                   rho=100, # range
                   f=0, # scaling factor for altitude difference
-                  znew = NULL, # altitude of new locations
-                  zgiven = NULL # altitude of given locations
+                  z.new = NULL, # altitude of new locations, vector [J]
+                  z.given = NULL # altitude of given locationsm vector [M]
                   ){
     # retrieve number of given and prediction locations
-    if(is.vector(xnew)){xnew <- matrix(xnew, ncol=2)}
-    N <- dim(xgiven)[1] # number of observations
-    J <- dim(xnew)[1] # number of predictions
+    if(is.vector(r.new)){r.new <- matrix(r.new, ncol=2)}
+    N <- dim(r.given)[1]
+    J <- dim(r.new)[1] 
     nu <- 3/2 # smoothness parameter of Matern-function
     R_e <- 6371000 # Earth's radius
-    #print(min(znew))
-    #print(min(zgiven))
 
     # calculate covariance matrix for given stations
-    dist_given <- matrix(0,nrow=N, ncol=N)
+    dist.given <- matrix(0,nrow=N, ncol=N)
     for (i in 1:N){
-        dist_given[,i] <- distHaversine(xgiven, xgiven[i,], r=R_e)/1000
+        dist.given[,i] <- sqrt( (distHaversine(r.given, r.given[i,], r=R_e)/1000)^2 + (f * abs(z.given-z.given[i]) /1000)^2)
     }
-    if (f){
-        d_update <- matrix(0,nrow=N, ncol=N)
-        for (i in 1:N){
-            d_update[,i] <- f * abs(zgiven-zgiven[i]) /1000
-        }
-        dist_given <- sqrt(dist_given^2 + d_update^2)
-    }
-
-    K_given <- (sigma^2) * matern(dist_given, rho/sqrt(2*nu), nu)
-    diag(K_given) <- diag(K_given) + 1e-5
-    K_inv <- solve(K_given)
+    K.given <- (sigma^2) * matern(dist.given, rho/sqrt(2*nu), nu)
+    diag(K.given) <- diag(K.given) + 1e-5
+    K.inv <- solve(K.given)
 
     # calculate covariance for new stations
-    dist_new <- matrix(0,nrow=J, ncol=J)
+    dist.new <- matrix(0,nrow=J, ncol=J)
     for (i in 1:J){
-        dist_new[,i] <- distHaversine(xnew, xnew[i,], r=R_e)/1000
+        dist.new[,i] <- sqrt((distHaversine(r.new, r.new[i,], r=R_e)/1000)^2 + (f * abs(z.new-z.new[i]) /1000)^2)
     }
-    if (f){
-        d_update <- matrix(0,nrow=J, ncol=J)
-        for (i in 1:J){
-            d_update[,i] <- f * abs(znew-znew[i]) /1000
-        }
-        dist_new <- sqrt(dist_new^2 + d_update^2)
-    }
-    K_new <- sigma^2*matern(dist_new, rho/sqrt(nu*2), nu)
+    K.new <- sigma^2*matern(dist.new, rho/sqrt(nu*2), nu)
 
     # calculate cross-distance matrix
     dist_cross <- matrix(0,nrow=N,ncol=J)
     for (i in 1:J){
-        dist_cross[,i] <- distHaversine(xgiven, xnew[i,], r=R_e)/1000
+        dist.cross[,i] <- sqrt((distHaversine(r.given, r.new[i,], r=R_e)/1000)^2 + (f * abs(z.given-z.new[i]) /1000)^2)
     }
-    if (f){
-        d_update <- matrix(0,nrow=N,ncol=J)
-        for (i in 1:J){
-            d_update[,i] <- f * abs(zgiven-znew[i]) /1000
-        }
-        dist_cross <- sqrt(dist_cross^2 + d_update^2)
-    }
-    K_cross <- sigma^2*matern(dist_cross,rho/sqrt(2*nu), nu)
+    K.cross <- sigma^2*matern(dist.cross,rho/sqrt(2*nu), nu)
 
     # calculate kriging estimate
     alpha_vec <- rep(alpha,max(N,J)) # expectation vectors
-    theta_new_mean <- alpha_vec[1:J] + t(K_cross) %*% K_inv %*% (theta - alpha_vec[1:N])
+    theta_new_mean <- alpha_vec[1:J] + t(K.cross) %*% K.inv %*% (theta - alpha_vec[1:N])
 
     # calculate conditional covariance
     theta_cov <- K_new - t(K_cross) %*% K_inv %*% K_cross
@@ -116,10 +71,10 @@ draw_cond_GRF <- function(theta, # observed values
     return(theta_new)
 }
 
-#coord_new <- mvrnorm(c(10,51), Sigma=matrix(c(2,0,0,2), nrow=2, ncol=2),n=10)
-
+##############################################################
+# Iterative interpolation procedure (Sect. 5.6 of manuscript)
+##############################################################
 ## Function for iteratively draw large samples. Has to be performed several times to obtain an estimate of the mean and variance at each location.
-
 draw_cond_large_GRF <- function(xgiven,
                                 theta,
                                 xnew,
@@ -129,7 +84,7 @@ draw_cond_large_GRF <- function(xgiven,
                                 f=0,
                                 zgiven=NULL,
                                 znew=NULL,
-                                nbatch=500,
+                                nbatch=500, # controls the number of prediction locations per iteration
                                 resample=FALSE){
     N <- dim(xnew)[1]
     M <- dim(xgiven)[1]
@@ -192,11 +147,3 @@ draw_cond_large_GRF <- function(xgiven,
     if(resample){theta_new <- theta_new[order(i_res)]} # resample back for output
     return(theta_new)
 }
-
-
-
-
-
-
-
-
